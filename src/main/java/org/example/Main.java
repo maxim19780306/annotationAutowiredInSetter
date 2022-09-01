7a69b7cd0078a57f46f63d683ab1fa1eff1ccd84
maxim19780306 <rm83788@gmail.com>
2022-09-02 00:23:16 +0300

    Внедрение зависимости через сеттер

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.config.ProjectConfig;
+import org.example.main.Person;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Person p = context.getBean(Person.class);
+
+        System.out.println("Person's name: " + p.getName());
+        System.out.println("Person's parrot: " + p.getParrot().getName());
+    }
+}
